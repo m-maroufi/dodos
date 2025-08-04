98785a39092e6ac2b34b444b8a1dd8d265fd6d80
Mehdi Maroufi <mehdimaroufi.it@gmail.com>
2025-08-04 11:55:07 +0330

    intiall: adding mikhak font and config css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
