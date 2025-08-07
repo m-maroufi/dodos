4133d86a348c3ef518757a6fedd6a0a3f0b8bfc6
Mehdi Maroufi <mehdimaroufi.it@gmail.com>
2025-08-07 21:23:27 +0330

    feat: refactor Toaster component for improved readability and consistency; remove unused imports in Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,7 +18,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { authContext } from "@/context/authContext";
 import { loginFormShema } from "@/lib/validateShema";
 import { sleep } from "@/lib/helper";
